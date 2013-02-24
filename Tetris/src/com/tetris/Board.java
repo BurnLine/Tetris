3e8d5c013f2745ca1761f56f3b3b077e44ee3ac4
Ján Lališ <burnlinesoft@gmail.com>
2013-02-24 21:01:12 +0100

    ok ide to

--- Tetris/src/com/tetris/Board.java
+++ Tetris/src/com/tetris/Board.java
@@ -3,10 +3,6 @@ package com.tetris;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
- * sdf test
- */
-
 public class Board {
 	private List<Node> boardMap;
 	private ArrayList<Node> removedLine;
